7514e84145c41bdfa4e969064f2e8df099e37621
Amrit Poudel <amritpoudel11@gmail.com>
2018-12-05 19:03:51 +0000

    click event for nav menu icon

--- portfolio.js
+++ portfolio.js
@@ -2,6 +2,5 @@ const navIcon = document.querySelector(".nav__icon");
 const navList = document.querySelector(".nav__list");
 
 navIcon.addEventListener("click", ()=>{
- alert("ok");
     navList.classList.toggle("toggle");
 });
