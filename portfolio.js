792caa5a0df9add9dd4095fdee6c46e6e945e634
Amrit Poudel <amritpoudel11@gmail.com>
2019-01-22 17:31:59 +0000

    added sweetalert for under construction alert

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,8 @@
+//alert
+Swal.fire(
+    "This Portfolio is under Construction. This is just the initial layout. Thank You. Cheers!!"
+  );
+//navBar
 const navIcon = document.querySelector(".nav__icon");
 const navList = document.querySelector(".nav__list");
 
